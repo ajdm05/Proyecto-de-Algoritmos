694c90b21a9be29d9162379b44bbfb74ea9be124
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2014-12-15 16:43:12 -0600

    Added methods to get the variable names in an expression

--- Simplificador_QM/Expression/expression.cpp
+++ Simplificador_QM/Expression/expression.cpp
@@ -9,6 +9,12 @@ bool Expression::evaluateExpression(map<string, bool> *values)
     return evaluateNode(root, values);
 }
 
+vector<string> Expression::getVariableNames()
+{
+    vector<string> names;
+    getVariables(root, &names);
+}
+
 bool Expression::evaluateNode(Node* node, map<string,bool>* values){
     if(node->getType() == AndNodeType){
         return evaluateNode( ((AndNode*)node)->getIzqNode() , values)
@@ -20,5 +26,35 @@ bool Expression::evaluateNode(Node* node, map<string,bool>* values){
         return ! evaluateNode( ((NotNode*)node)->getNode(), values);
     }else if(node->getType() == IdNodeType){
         return values->at( ((IdNode*)node)->getId() );
+    }
+    return false;
+}
+
+void Expression::getVariables(Node *node, vector<string> *vector)
+{
+    if(node->getType() == AndNodeType){
+        getVariables( ((AndNode*)node )->getIzqNode(), vector );
+        getVariables( ((OrNode*)node)->getDerNode(), vector);
+    }else if(node->getType() == OrNodeType){
+        getVariables( ((OrNode*)node)->getIzqNode(), vector);
+        getVariables( ((OrNode*)node)->getDerNode(), vector);
+    }else if(node->getType() == NotNodeType){
+        getVariables( ((NotNode*)node)->getNode(), vector);
+    }else if(node->getType() == IdNodeType){
+        pushUniqueVariable( ((IdNode*)node)->getId(), vector );
+    }
+}
+
+void Expression::pushUniqueVariable(string variableName, vector<string> *variableVector)
+{
+    bool found = false;
+    for(int i = 0; i < variableVector->size(); i++){
+        if(variableName.compare( variableVector->at(i) )){
+            found = true;
+            break;
+        }
+    }
+    if(!found){
+        variableVector->push_back(variableName);
     }
 }
--- Simplificador_QM/Expression/expression.h
+++ Simplificador_QM/Expression/expression.h
@@ -5,15 +5,21 @@
 #include "OrNode.h"
 #include "AndNode.h"
 #include <map>
+#include <vector>
+
+using namespace std;
 
 class Expression
 {
 public:
     Expression();
     bool evaluateExpression(map<string,bool> *values);
+    vector<string> getVariableNames();
 private:
     Node* root;
     bool evaluateNode(Node* node, map<string,bool>* values);
+    void getVariables(Node* node, vector<string>* vector);
+    void pushUniqueVariable(string, vector<string>*);
 };
 
 #endif // EXPRESSION_H
